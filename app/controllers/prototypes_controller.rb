1f9667f0a46b7bcfc0a6d33a81c3b4ba989e70bf
mugiwararf <ayumi04190703@gmail.com>
2023-03-20 16:15:41 +0900

    ログイン関連修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,8 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
     @prototypes = Prototype.all
   end
@@ -17,18 +21,15 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
     
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
@@ -37,7 +38,6 @@ class PrototypesController < ApplicationController
   end
 
  def destroy
-   @prototype = Prototype.find(params[:id])
    @prototype.destroy
    redirect_to root_path
  end
@@ -48,5 +48,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:name, :recommendation, :explanation, :category_id, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
 
 end
