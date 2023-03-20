b2b60629bef4d037a86f86cbce49459261006308
mugiwararf <ayumi04190703@gmail.com>
2023-03-20 15:23:45 +0900

    コメント機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -18,6 +18,9 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+    
   end
 
   def edit
