3f214875126943dd72ac322c0673339e38eabad0
mugiwararf <ayumi04190703@gmail.com>
2023-03-21 16:47:54 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
   resources :prototypes do
-    resources :comments, only: :create
+    resources :comments, only: [:create]
   end
+  post 'like/:id' => 'likes#create', as: 'create_like'
+  delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
   resources :users, only: :show
 end
